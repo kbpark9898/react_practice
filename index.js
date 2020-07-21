ed219c1262b35d2a91069d75fc02f2522ab18ebb
kbpark9898 <kbpark9898@gmail.com>
2020-07-21 16:46:50 +0900

    security patch

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ const db_url = "mongodb+srv://kbpark9898:<password>@boiler-plate.m8yyl.mongodb.n
 const {User} = require('./models/user')
 const bodyParser = require('body-parser')
 const mongoose = require("mongoose")
+const key = require('./config/key')
 app.use(bodyParser.urlencoded({extended:'true'}))
 app.use(bodyParser.json())
-mongoose.connect("mongodb://kbpark9898:park0130@boiler-plate-shard-00-00.m8yyl.mongodb.net:27017,boiler-plate-shard-00-01.m8yyl.mongodb.net:27017,boiler-plate-shard-00-02.m8yyl.mongodb.net:27017/boiler-plate?ssl=true&replicaSet=atlas-nkbm0s-shard-0&authSource=admin&retryWrites=true&w=majority", {
+mongoose.connect(key.mongoURI, {
     useNewUrlParser : "true", useUnifiedTopology:"true", useCreateIndex:"true", useFindAndModify:"false"
 }).then(() => console.log("cunnect success"))
   .catch(err => console.log(err))
-app.get('/', (req, res) => res.send('안녕하세요!'))
+app.get('/', (req, res) => res.send('안녕하세요! 연결되나요?'))
 
 app.post('/register', (req, res)=>{
-  //회원가입에 필요한 정보를 client 에게서 받아오면 데이터베이스에 넣어준다.
-  //7강 8분부터 이어서 시청할것
-  //다시 커밋해도 유저 정보 저장 안되어있나?
-
+  const user = new User(req.body)
+  user.save((err,userInfo)=>{
+    if(err) return res.json({success:false, err})
+    return res.status(200).json({success:true})
+  })
 })
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
